f5e93bea2c71a5f89a6950351a14bcf33f96fa15
ragrok <ragrokli@hotmail.com>
2019-04-19 01:08:45 +0800

    spring boot 热部署

---
+++ Spring-service/src/main/java/cn/com/github/service/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package cn.com.github.service.impl;
+
+import cn.com.github.domain.entity.User;
+import cn.com.github.repo.dao.UserRepository;
+import cn.com.github.service.inter.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public List<User> list() {
+        return repository.findAll();
+    }
+
+    @Override
+    public User findByName(String name) {
+        return repository.findByUserName(name);
+    }
+}
---
+++ Spring-service/src/main/java/cn/com/github/service/inter/UserService.java
@@ -0,0 +1,12 @@
+package cn.com.github.service.inter;
+
+import cn.com.github.domain.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public List<User> list();
+
+    public User findByName(String name);
+}
