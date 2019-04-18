872a6b3fb43b4d7033afd1ebbbebb5ee8e4b5d3b
ragrok <ragrokli@hotmail.com>
2019-04-18 22:01:00 +0800

    spring boot 热部署

--- SpringDemo1/src/main/java/cn/com/github/demo1/config/AppConfig.java
+++ SpringDemo1/src/main/java/cn/com/github/demo1/config/AppConfig.java
@@ -3,13 +3,17 @@ package cn.com.github.demo1.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 /**
  * @ClassName Config
  * @AUTHOR ragrokli
  * @DATE 2019/4/18 10:46
  **/
-@PropertySource(value = {"classpath:log4j.properties", "classpath:application.properties", "classpath:user.properties"})
+@PropertySources({
+        @PropertySource("classpath:log4j.properties"),
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:user.properties")})
 @ComponentScan(basePackages = "cn.com.github")
 @Configuration
 public class AppConfig {
