11b31a35d5247e519b1804f2ef8c228521078ae5
ragrokli <ragrokli@hotmail.com>
2019-04-19 17:21:38 +0800

    注解文件配置

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 学习spring 
 # 搭建spring boot 多项目
 
+
+1. 自定义注解
+https://www.cnblogs.com/peida/archive/2013/04/24/3036689.html
+
--- Spring-web/src/test/java/cn/com/github/test1/test1/Test1.java
+++ Spring-web/src/test/java/cn/com/github/test1/test1/Test1.java
@@ -2,6 +2,7 @@ package cn.com.github.test1.test1;
 
 import cn.com.github.domain.entity.User;
 import cn.com.github.service.inter.UserService;
+import cn.com.github.service.util.FruitInfoUtil;
 import cn.com.github.test1.base.BaseTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -19,7 +20,7 @@ public class Test1 extends BaseTest {
     @Test
     public void test() throws Exception {
         Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+        DateFormat dateFormat = DateFormat.getDateTimeInstance();
         String formattedDate = dateFormat.format(date);
 
         userService.save(new User("aa1", "aa@126.com", "aa", "aa123456", formattedDate));
@@ -29,5 +30,10 @@ public class Test1 extends BaseTest {
         //Assert.assertEquals(9, userRepository.findAll().size());
         //Assert.assertEquals("bb", userRepository.findByUserNameOrEmail("bb", "cc@126.com").getNickName());
         //userRepository.delete(userRepository.findByUserName("aa1"));
+    }
+
+    @Test
+    public void test1() {
+        FruitInfoUtil.getFruitInfo(Apple.class);
     }
 }
