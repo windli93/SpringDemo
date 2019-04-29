a93048a2cf3626fcb62d215cb39a8f783117caca
ragrok <ragrokli@hotmail.com>
2019-04-29 23:44:09 +0800

    spring boot 热部署

--- README.md
+++ README.md
@@ -5,4 +5,16 @@
 
 1. 自定义注解
 https://www.cnblogs.com/peida/archive/2013/04/24/3036689.html
+2. redis 
+springboot 和redis项目整合：https://www.cnblogs.com/skyessay/p/6485187.html 
+redis 专栏学习：https://blog.csdn.net/xuhuanfeng232/article/category/8087082  
+
+
+# 自己机器：
+root 1234   vhr
+# 公司机器：
+root root 数据库 test
+
+
+
 
--- Spring-web/src/test/java/cn/com/github/test1/test1/Test1.java
+++ Spring-web/src/test/java/cn/com/github/test1/test1/Test1.java
@@ -1,15 +1,21 @@
 package cn.com.github.test1.test1;
 
 import cn.com.github.domain.entity.User;
+import cn.com.github.service.config.JedisConfig;
 import cn.com.github.service.inter.UserService;
 import cn.com.github.service.util.FruitInfoUtil;
 import cn.com.github.test1.base.BaseTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class Test1 extends BaseTest {
@@ -36,4 +42,32 @@ public class Test1 extends BaseTest {
     public void test1() {
         FruitInfoUtil.getFruitInfo(Apple.class);
     }
+
+    @Test
+    public void test2() {
+
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxWaitMillis(60 * 1000);
+        config.setMaxIdle(50);
+        config.setTestOnBorrow(true);
+        config.setMinIdle(20);
+        config.setMaxTotal(200);
+
+        JedisPool jedisPool = new JedisPool(config, "localhost", 6379, 20000, "1234");
+
+        try (Jedis jedis = jedisPool.getResource()) {
+            jedis.set("hello", "world");
+            jedis.hset("stu", "name", "xavier");
+            jedis.hset("stu", "age", "23");
+            jedis.lpush("myList", "1", "2", "3");
+            jedis.sadd("names", "xavier", "john", "tom");
+            jedis.zadd("scoreboard", 99, "xavier");
+            Map<String, Double> scoreboard = new HashMap<>();
+            scoreboard.put("tom", 100.0);
+            scoreboard.put("john", 101.0);
+            jedis.zadd("scoreboard", scoreboard);
+        }
+
+    }
+
 }
