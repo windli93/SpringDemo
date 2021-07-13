5638ecd51924507a4e03f1490d4af2b24f6196c2
wind.li <wind.li@yeahmobi.com>
2021-07-13 22:34:15 +0800

    提交代码

---
+++ Spring-Chat/src/main/java/cn/com/github/nio/NioTest1.java
@@ -0,0 +1,34 @@
+package cn.com.github.nio;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+@Slf4j
+public class NioTest1 {
+
+    @Test
+    public void byteBuffTest1() throws Exception {
+        FileChannel channel = new FileInputStream(this.getClass().getClassLoader().getResource("data.txt").getPath()).getChannel();
+        //准备缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        while (true) {
+            int length = channel.read(buffer);
+            log.info("length:{}", length);
+            if (length == -1) {
+                return;
+            }
+            //切换写模式
+            buffer.flip();
+            while (buffer.hasRemaining()) {
+                byte b = buffer.get();
+                log.info("data:{}", (char) b);
+            }
+            //切换读模式
+            buffer.clear();
+        }
+    }
+}
