c921e2430002e2bd755e1f7b9c1dddbc6ee8f348
windli <wind@plushe.com>
2021-07-20 16:01:12 +0800

    提交代码

--- Spring-Netty/src/main/java/cn/com/github/nio/NioServer.java
+++ Spring-Netty/src/main/java/cn/com/github/nio/NioServer.java
@@ -22,9 +22,10 @@ public class NioServer {
     @Test
     public void test1() throws IOException {
          //使用Nio来理解阻塞模式
-        ByteBuffer buffer = ByteBuffer.allocate(16);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
         //创建服务器
         ServerSocketChannel  channel = ServerSocketChannel.open();
+        channel.configureBlocking(false);
         //绑定监听器
         channel.bind(new InetSocketAddress(10006));
         //建立连接
@@ -32,15 +33,20 @@ public class NioServer {
         while (true){
             //accept建立与客户端连接，SocketChannel用来与客户端通信
             SocketChannel channel1 = channel.accept();
-            channels.add(channel1);
+            if (channel1 != null){
+                log.debug("connected...{}",channel1);
+                channel1.configureBlocking(false);
+                channels.add(channel1);
+            }
             for (SocketChannel channel2: channels){
                 //接收客户端发送的数据
-                log.debug("before read...{}",channel);
-                channel2.read(buffer);
-                buffer.flip();
-                log.info("Buffer data:{}",buffer.array());
-                buffer.clear();
-                log.debug("after read...{}",channel);
+                int read = channel2.read(buffer);
+                if (read > 0){
+                    buffer.flip();
+                    log.info("Buffer data:{}",buffer.array());
+                    buffer.clear();
+                    log.debug("after read...{}",channel);
+                }
             }
         }
     }
