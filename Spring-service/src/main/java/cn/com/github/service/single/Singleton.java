8ef558da0187723972cf8c02f058619b68f220c6
wind.li <wind.li@yeahmobi.com>
2021-07-26 08:32:38 +0800

    提交代码

---
+++ Spring-service/src/main/java/cn/com/github/service/single/Singleton.java
@@ -0,0 +1,21 @@
+package cn.com.github.service.single;
+
+public class Singleton {
+
+    private volatile static Singleton singleton;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getSingleton(){
+        if (singleton == null){
+            synchronized (Singleton.class){
+                if (singleton == null){
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
